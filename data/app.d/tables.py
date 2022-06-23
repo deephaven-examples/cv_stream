c151a82c744ee145a145272d88d48ae140ee9259
Jeremiah Cheng <99699304+Jeremiahcheng1206@users.noreply.github.com>
2022-06-23 16:20:59 -0400

    Add files via upload

---
+++ data/app.d/tables.py
@@ -0,0 +1,14 @@
+import time
+time.sleep(30)
+from deephaven import kafka_consumer as ck
+from deephaven.stream.kafka.consumer import TableType, KeyValueSpec
+from deephaven import dtypes as dht
+
+relation_table = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'relation', key_spec=KeyValueSpec.IGNORE, value_spec=ck.json_spec([
+    ('name', dht.string)
+    ]), table_type = TableType.append())
+
+attendance_table = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'attendance', key_spec=KeyValueSpec.IGNORE, value_spec=ck.json_spec([
+    ('name', dht.string)
+    ]), table_type = TableType.append())
+
