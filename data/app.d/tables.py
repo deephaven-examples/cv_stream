8bd99f75ca7af0e83ff74f05605c36eb8dbff184
Jeremiah Cheng <99699304+Jeremiahcheng1206@users.noreply.github.com>
2022-07-01 09:38:50 -0400

    Update tables.py

--- data/app.d/tables.py
+++ data/app.d/tables.py
@@ -4,11 +4,11 @@ from deephaven import kafka_consumer as ck
 from deephaven.stream.kafka.consumer import TableType, KeyValueSpec
 from deephaven import dtypes as dht
 
-relation_table = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'relation', key_spec=KeyValueSpec.IGNORE, value_spec=ck.json_spec([
+relation_table = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'character_relation', key_spec=KeyValueSpec.IGNORE, value_spec=ck.json_spec([
     ('name', dht.string)
     ]), table_type = TableType.append())
 
-attendance_table = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'attendance', key_spec=KeyValueSpec.IGNORE, value_spec=ck.json_spec([
+attendance_table = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'character_attendance', key_spec=KeyValueSpec.IGNORE, value_spec=ck.json_spec([
     ('name', dht.string)
     ]), table_type = TableType.append())
 
